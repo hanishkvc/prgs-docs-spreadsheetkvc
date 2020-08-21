b12b93f1b5dbba62649b5622b043eb8a6b03db5e
hanishkvc <hanishkvc@gmail.com>
2020-08-21 11:27:30 +0530

    Initial skeleton for a matrix of cells

--- spreadkvc.py
+++ spreadkvc.py
@@ -4,6 +4,12 @@
 
 import curses
 
+me = {
+        'cw': 5, 'ch': 1,
+        'nc': 5, 'nr': 5, 
+        }
+
+
 def cstart():
     stdscr = curses.initscr()
     curses.noecho()
@@ -12,13 +18,24 @@ def cstart():
     stdscr.clear()
     return stdscr
 
+
 def cend(stdscr):
     curses.nocbreak()
     stdscr.keypad(False)
     curses.echo()
     curses.endwin()
 
+
+def cdraw(stdscr):
+    for i in range(me['nc']):
+        stdscr.addstr(0, i*me['cw'], "{}".format(i))
+    for i in range(me['nr']):
+        stdscr.addstr(i, 0, "{}".format(i))
+    stdscr.refresh()
+
+
 stdscr=cstart()
+cdraw(stdscr)
 stdscr.getkey()
 cend(stdscr)
 
