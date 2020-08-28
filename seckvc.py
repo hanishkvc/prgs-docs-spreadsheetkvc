d84a644ea91068ca8b7f6e18bf327b2713a38820
hanishkvc <hanishkvc@gmail.com>
2020-08-29 00:55:23 +0530

    AuthenticatedEncryption: aes cbc enc with mac skeleton

--- seckvc.py
+++ seckvc.py
@@ -10,20 +10,43 @@ from cryptography.hazmat.primitives.hashes import SHA256
 from cryptography.hazmat.primitives.hmac import HMAC
 from cryptography.hazmat.backends import default_backend
 #cryptography.hazmat.primitives.padding
+import os
 
 
-aes=AES(b'KEY_AESALGOKEYSIZE')
-cbc = CBC(b'RANDOM_IV_SYMALGOKEYSIZE')
-aesCbc=Cipher(aes,cbc,default_backend())
-aesCbcEnc=aes_cbc.encryptor()
+def aes_cbc_enc(aesKey, plainMsg):
+    '''
+    Do a encrypt then mac operation on given message.
+    '''
+    ### Prepare for encryption
+    blockLen = len(aesKey)
+    iv = os.urandom(blockLen)
+    aes=AES(aesKey)
+    cbc = CBC(iv)
+    aesCbc=Cipher(aes,cbc,default_backend())
+    aesCbcEnc=aesCbc.encryptor()
+    random0thBlock = os.urandom(blockLen)
+    # This allows iv to be discarded
+    # so also while decrypting discard the 0th block
+    plainMsg = random0thBlock + plainMsg
+    padLen = blockLen - (len(plainMsg)%blockLen)
+    # Space padding will do in this particular usecase
+    for i in range(padLen):
+        plainMsg += ' '
+    ### do encrypt
+    aesCbcEnc.update(plainMsg)
+    encMsg = aesCbcEnc.finalize()
+    ### Prepare for mac
+    sha256=SHA256()
+    hmac=HMAC(aesKey,sha256,default_backend())
+    ### do mac
+    hmac.update(encMsg)
+    macMsg = hmac.finalize()
+    return encMsg, macMsg
 
-aesCbcEnc.update(b'123')
-aesCbcEnc.update(b'456')
-aesCbcEnc.update(b'789')
-aesCbcEnc.finalize()
 
-sha256=SHA256()
-hmac=HMAC(b'TheKey',sha256,default_backend())
+print(aes_cbc_enc(b'0123456789abcdef', "hello world"))
 
 
 
+
+# vim: set sts=4 expandtab: #
