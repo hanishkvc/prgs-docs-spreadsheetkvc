20fe4513774d243828e41e758dd0607fad2f24a0
hanishkvc <hanishkvc@gmail.com>
2020-08-30 10:50:29 +0530

    cui: _extend_str helper, dlg finetuning including clear
    also fix a oversight wrt dprint

--- cuikvc.py
+++ cuikvc.py
@@ -13,7 +13,7 @@ GLOGFILE=None
 GERRFILE=None
 
 
-def dprint(sMsg, file=file, end="\n"):
+def dprint(sMsg, file=GLOGFILE, end="\n"):
     '''
     debug print to specified file, if not None
     '''
@@ -47,6 +47,9 @@ def cend(scr):
 def cellstr(scr, y, x, msg, attr, clipToScreen=True):
     '''
     Display message onlyy if it is in the current display viewport
+
+    if clipToScreen is true, then clip message such that it fits the
+    screen horizontally.
     '''
     if ((x < 0) or (x >= me['scrCols'])) or ((y < 0) or (y >= me['scrRows'])) :
         return
@@ -60,13 +63,29 @@ def cellstr(scr, y, x, msg, attr, clipToScreen=True):
     scr.addstr(y, x, msg, attr)
 
 
-def dlg(scr, msgs, y=0, x=0, attr=curses.A_NORMAL, border=False, newwin=False):
+def _extend_str(strIn, length, filler=' '):
+    '''
+    Extended the string to given length, if its smaller, by using given filler.
+    '''
+    strLen = len(strIn)
+    strOut = strIn
+    for i in range(length-strLen):
+        strOut += filler
+    return strOut
+
+
+def dlg(scr, msgs, y=0, x=0, attr=curses.A_NORMAL, border=False, newwin=False, clear=True):
     '''
     Show a simple dialog, with the messages passed to it.
     And return a keypress from the user.
+
     If location not given, then show at top left corner.
+    If border, then show a border covering full width above and below 0th message.
+    If clear, clear the screen after getting input from user.
+        also clear the screen till borderWidth for each line printed.
+    borderWidth is the length of the longest message+2.
     '''
-    if border or newwin:
+    if border or newwin or clear:
         borderWidth = 0
         for i in range(len(msgs)):
             msgLen = len(msgs[i])
@@ -76,6 +95,9 @@ def dlg(scr, msgs, y=0, x=0, attr=curses.A_NORMAL, border=False, newwin=False):
         revAttr = curses.A_REVERSE
         if attr == curses.A_REVERSE:
             revAttr = curses.A_NORMAL
+    if clear:
+        for i in range(len(msgs)):
+            msgs[i] = _extend_str(msgs[i], borderWidth, " ")
     if newwin:
         scr = curses.newwin(len(msgs), borderWidth, y, x)
         scr.clear()
@@ -84,8 +106,7 @@ def dlg(scr, msgs, y=0, x=0, attr=curses.A_NORMAL, border=False, newwin=False):
     if border:
         tX = x+1
         borderStr = ""
-        for i in range(borderWidth):
-            borderStr += " "
+        borderStr = _extend_str(borderStr, borderWidth, " ")
         cellstr(scr, y, x, borderStr, revAttr)
         cellstr(scr, y+1, tX, msgs[0], attr)
         cellstr(scr, y+2, x, borderStr, revAttr)
@@ -97,7 +118,8 @@ def dlg(scr, msgs, y=0, x=0, attr=curses.A_NORMAL, border=False, newwin=False):
     for i in range(1, len(msgs)):
         cellstr(scr, tY+i, tX, msgs[i], attr)
     got = scr.getch()
-    scr.clear()
+    if clear:
+        scr.clear()
     return got
 
 
