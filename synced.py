cecb864c4884269ae9430950dad31a2c05ffb877
hanishkvc <hanishkvc@gmail.com>
2020-09-11 22:57:05 +0530

    Synced module: Initial skeleton - build fwd link
    Build fwd link from each cell with a =expression with cell addresses
    in them.

---
+++ synced.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# Help keep cells in sync
+# HanishKVC, 2020
+#
+
+
+import parsekvc as parse
+
+
+me = None
+
+
+'''
+Sync module maintains a list of both forward and reverse links.
+'''
+
+
+def init():
+    me['fwdLinks'] = dict()
+    me['revLinks'] = dict()
+
+
+def cell_updated(cellKey, sContent):
+    '''
+    Update the fw and reverse links associated with each cell
+
+    NOTE: For now it maintains a expanded list of cells. TO keep the
+    logic which depends on this at other places simple and stupid.
+    '''
+    cellFwdLink = me['fwdLinks'].get(cellKey)
+    if cellFwdLink == None:
+        cellFwdLink = []
+    lCellAddrs = parse.get_celladdrs_incranges(sContent)
+    for cellAddrPlus in lCellAddrs:
+        if (len(cellAddrPlus) == 1):
+            bCellAddr, key = _celladdr_valid(cellAddrPlus[0])
+            if not bCellAddr:
+                print("sync.cellUpdated:WARN:{}:{}".format(sContent, key))
+                continue
+            cellFwdLink.append(key)
+        elif (len(cellAddrPlus) == 2):
+            bCellAddr, key1 = _celladdr_valid(cellAddrPlus[0])
+            if not bCellAddr:
+                print("sync.cellUpdated:WARN:{}:{}".format(sContent, key))
+                continue
+            bCellAddr, key2 = _celladdr_valid(cellAddrPlus[1])
+            if not bCellAddr:
+                print("sync.cellUpdated:WARN:{}:{}".format(sContent, key))
+                continue
+            for r in range(key1[0], key2[0]+1):
+                for c in range(key1[1], key2[1]+1):
+                    cellFwdLink.append((r,c))
+
+
+
+
+# vim: set sts=4 expandtab: #
