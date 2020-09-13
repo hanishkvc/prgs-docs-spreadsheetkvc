11e824e4be369ba38888d77f760644f391f5f6d4
hanishkvc <hanishkvc@gmail.com>
2020-09-13 12:38:03 +0530

    Insert a random number of pseudo cell addresses
    For general text files with = prefixed to each line to force
    the parser to try and interpret it as an expression where cell
    addresses could be there, I found that my internal logic based
    parser is 2 times as fast as the re based parser logic.
    
    Just to verify that both parser are handling the parsing of
    cell addresses properly, now I am adding some cell addresses
    randomly into the lines.

--- tests/filelinemanipulate.py
+++ tests/filelinemanipulate.py
@@ -3,12 +3,29 @@
 # HanishKVC, 2020
 #
 
+
 import sys
+import random
+
 
 f = open(sys.argv[1])
 
 for l in f:
-	print("={}".format(l))
+	if l[-1] == '\n':
+		l = l[:-1]
+	aRand1 = random.randint(0,26)
+	aRand2 = random.randint(0,26)
+	iCellAddrs = (aRand1%4)
+	if iCellAddrs == 0:
+		print("={}".format(l))
+		continue
+	lLen = len(l)
+	iOffset = int(lLen/iCellAddrs)
+	lOut = ""
+	for i in range(iCellAddrs):
+		lOut += (l[iOffset*i:iOffset*(i+1)] + " {}{}{} ".format(chr(ord('A')+aRand1), chr(ord('A')+aRand2), aRand1))
+	lOut += l[iOffset*(i+1):]
+	print("={}".format(lOut))
 
 f.close()
 
