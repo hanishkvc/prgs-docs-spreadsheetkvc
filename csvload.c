ccf92118a44ed53488bee61737642cbf9b6e429d
hanishkvc <hanishkvc@gmail.com>
2020-09-25 18:34:58 +0530

    csvload get_celladdrs_incranges initial code
    This implements get_token and cell address (inc ranges) in a single
    logic.
    
    As tokenising and extracting cell addresses (including ranges) is
    one of the things which is taking more time is dependency links
    building, so trying out this integrated logic to see if it is faster.

--- csvload.c
+++ csvload.c
@@ -93,6 +93,7 @@ static str_empty(const char *str) {
 
 }
 
+
 //RE_CAINCR = re.compile("(.*?)([$]?[a-zA-Z]+[$]?[0-9]+[ ]*[:]?)(.*?)")
 PyDoc_STRVAR(
     get_celladdrs_incranges_fromre_doc,
@@ -123,6 +124,152 @@ static PyObject* get_celladdrs_incranges_fromre(PyObject *self, PyObject *args) 
         PyObject *raw = PyList_GetItem(rawList, i);
         PyObject *ca = PyTuple_GetItem(raw, 1);
         char *sCA = PyUnicode_AsUTF8AndSize(ca, &caLen);
+        //printf("DBUG:%d:%s\n", i, sCA);
+        // Remove any space in ca
+        iD = 0;
+        for(iS = 0; iS < caLen; iS++) {
+            if (sCA[iS] == ' ')
+                continue;
+            sCleanCA[iD] = sCA[iS];
+            iD += 1;
+        }
+        sCleanCA[iD] = 0;
+        if (iD > 0) {
+            cCleanCALast = sCleanCA[iD-1];
+            // Remove ':' if any at end
+            if (cCleanCALast == ':') {
+                iD -= 1;
+                sCleanCA[iD] = 0;
+            }
+        } else {
+            cCleanCALast = 0;
+        }
+        if (bInCARange) {
+            PyObject *preCA = PyTuple_GetItem(raw, 0);
+            char *sPreCA = PyUnicode_AsUTF8AndSize(preCA, &caLen);
+            //printf("DBUG:%d:%s\n", i, sPreCA);
+            //Py_DECREF(preCA);
+            bool bCellAddrRangeOk = true;
+            for(j = 0; j < caLen; j++) {
+                if (sPreCA[j] != ' ') {
+                    bCellAddrRangeOk = false;
+                }
+            }
+            if (bCellAddrRangeOk) {
+                //caList[-1].append(pCA)
+                int iEnd = PyList_Size(caList)-1;
+                if (iEnd == -1) {
+                    caRange = PyList_New(0);
+                    PyList_Append(caRange, PyUnicode_FromString(sCleanCA));
+                    PyList_Append(caList, caRange);
+                } else {
+                    caRange = PyList_GetItem(caList, iEnd);
+                    PyList_Append(caRange, PyUnicode_FromString(sCleanCA));
+                }
+            } else {
+                caRange = PyList_New(0);
+                PyList_Append(caRange, PyUnicode_FromString(sCleanCA));
+                PyList_Append(caList, caRange);
+            }
+            bInCARange = false;
+        } else {
+            caRange = PyList_New(0);
+            PyList_Append(caRange, PyUnicode_FromString(sCleanCA));
+            PyList_Append(caList, caRange);
+        }
+        if (cCleanCALast == ':') {
+            PyObject *postCA = PyTuple_GetItem(raw, 2);
+            char *sPostCA = PyUnicode_AsUTF8AndSize(postCA, &caLen);
+            //Py_DECREF(postCA);
+            bool bCellAddrRangeOk = true;
+            for(j = 0; j < caLen; j++) {
+                if (sPostCA[j] != ' ') {
+                    bCellAddrRangeOk = false;
+                }
+            }
+            if (bCellAddrRangeOk)
+                bInCARange = true;
+        }
+        //Py_DECREF(raw);
+        //Py_DECREF(ca);
+    }
+    return caList;
+}
+
+
+//RE_CAINCR = re.compile("(.*?)([$]?[a-zA-Z]+[$]?[0-9]+[ ]*[:]?)(.*?)")
+PyDoc_STRVAR(
+    get_celladdrs_incranges_doc,
+    "cellAddrsList = get_celladdrs_incranges(sIn)\n"
+    "--\n\n"
+    "Parse the given string and extract cell addresses (including ranges) in them.\n"
+    "\n"
+    "NOTE: This doesnt ignore contents of quoted text within the string.\n");
+static PyObject* get_celladdrs_incranges(PyObject *self, PyObject *args) {
+    PyObject *caList = PyList_New(0);
+    PyObject *caRange = NULL;
+    //int caListCnt = 0;
+    bool bInCARange = false;
+    long int caLen;
+    int iD, iS, j;
+    char sCleanCA[32];
+    int cCleanCALast;
+    char *sIn, *sCur;
+    int iC;
+
+    if (!PyArg_ParseTuple(args, "s", &sIn)) {
+        return NULL;
+    }
+    //printf("DBUG:GotList ofSize[%d] fromArgs\n", listLen);
+    iS = 0;
+    iToken = 0; // iToken = 0 (Not in token), 1 ($ found), 2 (alpha part), 3(num part)
+    while (true) {
+        c = sIn[iS];
+        if (c == 0) {
+            break;
+        }
+        if (isalpha(c)) {
+            if (iToken == 0) {
+                iToken = 2;
+                iC = 0;
+                sCur[iC] = c;
+                iC += 1;
+            } else if ((iToken == 1) || (iToken == 2)) {
+                iToken = 2;
+                sCur[iC] = c;
+                iC += 1;
+            } else { // Cant get alpha after numerals in cell addr
+                iToken = 0;
+            }
+        } else if (isnumeric(c)) {
+            if ((iToken == 2) || (iToken == 3)) {
+                iToken = 3;
+                sCur[iC] = c;
+                iC += 1;
+            } else {
+                iToken = 0;
+            }
+        } else if (c == '$') {
+            if (iToken == 0) {
+                iToken = 1;
+                iC = 0;
+                sCur[iC] = c;
+                iC += 1;
+            } else {
+                iToken = 0;
+            }
+        } else {
+            if (iToken == 3) { // Found a cell addr
+                sCur[iC] = 0;
+                caRange = PyList_New(0);
+                PyList_Append(caRange, PyUnicode_FromString(sCur));
+                PyList_Append(caList, caRange);
+            }
+            iToken = 0;
+        }
+        //
+        // BElow is from the celladdrs_incranges_fromre, which will be removed after picking some parts later.
+        //
         //printf("DBUG:%d:%s\n", i, sCA);
         // Remove any space in ca
         iD = 0;
